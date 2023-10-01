576edcdf0f7be60f51ca5d9328f3f2befbc057c3
Steve Schilz <steve.schilz@gmail.com>
2023-10-01 08:15:20 -0700

    Approach1-ComponentOwnsState Callback to main component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(props.initVal);
+
+  function handleDecrease() {
+    if (count >= -365) {
+      setCount((c) => c - 1);
+      props.onChange(count);
+      console.log("handle decrease " + (count - 1));
+    }
+  }
+
+  function handleIncrease() {
+    if (count <= 365) {
+      setCount((c) => c + 1);
+      props.onChange(count);
+      console.log("handle Increase " + (count + 1));
+    }
+  }
+
+  return (
+    <div>
+      <button className="App-button" onClick={handleDecrease}>
+        -
+      </button>
+      {props.text} : {count}
+      <button className="App-button" onClick={handleIncrease}>
+        +
+      </button>
+    </div>
+  );
+}
 
 function App() {
+  var step = 1;
+  var count = 0;
+
+  // Note: thes are vanilla javascript functions, not
+  //       react components, therefore parameter c is
+  //       simply the number, not a 'props' object
+  function onStepChanged(c) {
+    step = c;
+    console.log("onStepChanged: " + step);
+  }
+
+  function onCountChanged(c) {
+    count = c;
+    console.log("onCountChanged: " + c);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p></p>
+        <Counter text="Step" initVal={step} onChange={onStepChanged} />
+        <Counter text="Count" initVal={count} onChange={onCountChanged} />
       </header>
     </div>
   );
