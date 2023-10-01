940b07a80120626b1fb4749e11d09615557a93ea
Steve Schilz <steve.schilz@gmail.com>
2023-10-01 08:46:07 -0700

    Approach2: State in App component, plain javascript callbacks used to set state
    This approach is much cleaner, I'm very happy with it.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
+// SpinButton Component
+//
+// Parameters
+// props.val = current value of item
+// props.onChange = plain javascript function to call when changing item
+//                  (parameter = stepSize)
+// props.stepSize = integer value how much to change component on increment/decremanet
 function Counter(props) {
-  const [count, setCount] = useState(props.initVal);
-
   function handleDecrease() {
-    if (count >= -365) {
-      setCount((c) => c - 1);
-      props.onChange(count);
-      console.log("handle decrease " + (count - 1));
+    if (props.val >= -365) {
+      props.onChange(-1 * props.stepSize);
     }
   }
 
   function handleIncrease() {
-    if (count <= 365) {
-      setCount((c) => c + 1);
-      props.onChange(count);
-      console.log("handle Increase " + (count + 1));
+    if (props.val <= 365) {
+      props.onChange(+1 * props.stepSize);
     }
   }
 
@@ -26,7 +27,7 @@ function Counter(props) {
       <button className="App-button" onClick={handleDecrease}>
         -
       </button>
-      {props.text} : {count}
+      {props.text} : {props.val}
       <button className="App-button" onClick={handleIncrease}>
         +
       </button>
@@ -35,20 +36,20 @@ function Counter(props) {
 }
 
 function App() {
-  var step = 1;
-  var count = 0;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
 
-  // Note: thes are vanilla javascript functions, not
-  //       react components, therefore parameter c is
+  // Note: these callbacks are vanilla javascript functions,
+  //       not react components, therefore parameter is
   //       simply the number, not a 'props' object
-  function onStepChanged(c) {
-    step = c;
-    console.log("onStepChanged: " + step);
+  function onStepChanged(valueToAdd) {
+    setStep((s) => s + valueToAdd);
+    console.log("onStepChanged " + step);
   }
 
-  function onCountChanged(c) {
-    count = c;
-    console.log("onCountChanged: " + c);
+  function onCountChanged(valueToAdd) {
+    setCount((c) => c + valueToAdd);
+    console.log("onCountChanged: " + count);
   }
 
   return (
@@ -56,8 +57,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p></p>
-        <Counter text="Step" initVal={step} onChange={onStepChanged} />
-        <Counter text="Count" initVal={count} onChange={onCountChanged} />
+        <Counter text="Step" val={step} onChange={onStepChanged} stepSize={1} />
+        <Counter
+          text="Count"
+          val={count}
+          onChange={onCountChanged}
+          stepSize={step}
+        />
       </header>
     </div>
   );
